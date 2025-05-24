a7507c5acbc320359136454e2c1b88defcc5749d
preethamnandyala <npreethamnandyala@gmail.com>
2025-05-24 13:38:47 -0400

    Initial commit

---
+++ src/main/java/com/pm/simpleweatherapp/SimpleWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.pm.simpleweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWeatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWeatherAppApplication.class, args);
+    }
+
+}
